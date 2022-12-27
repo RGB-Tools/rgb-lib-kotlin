0a984ef0c70a868737de8ac47533932f3b824834
Nicola Busanello <nicola.busanello@gmail.com>
2022-12-27 14:34:42 +0100

    bump to 0.1.6-SNAPSHOT

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -52,7 +52,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "org.rgbtools"
                 artifactId = "rgb-lib-android"
-                version = "0.1.4-SNAPSHOT"
+                version = "0.1.6-SNAPSHOT"
                 from(components["release"])
                 pom {
                     name.set("rgb-lib-android")
