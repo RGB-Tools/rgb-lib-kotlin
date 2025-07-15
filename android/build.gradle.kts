8cf410ba85c553f8f70b0cd80812418632506f74
Nicola Busanello <nicola.busanello@gmail.com>
2025-07-15 23:43:20 +0200

    release version 0.3.0-beta.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -56,7 +56,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "org.rgbtools"
                 artifactId = "rgb-lib-android"
-                version = "0.3.0-beta.1-SNAPSHOT"
+                version = "0.3.0-beta.1"
                 from(components["release"])
                 pom {
                     name.set("rgb-lib-android")
--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ signing {
 }
 
 group = "org.rgbtools"
-version = "0.3.0-beta.1-SNAPSHOT"
+version = "0.3.0-beta.1"
 
 nexusPublishing {
     repositories {
